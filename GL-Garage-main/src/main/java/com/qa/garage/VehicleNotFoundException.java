750716762016d0712a4653884403098b56cc0eaf
Olivia Kelly <126669337+okelly54@users.noreply.github.com>
2023-03-21 12:20:48 +0000

    Add files via upload

---
+++ GL-Garage-main/src/main/java/com/qa/garage/VehicleNotFoundException.java
@@ -0,0 +1,14 @@
+package com.qa.garage;
+
+public class VehicleNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public VehicleNotFoundException() {
+		String response = "Nothing found";
+	}
+
+}
